cf6490d99e8c99e30a20f5644debb78faabb3f30
Daniel Rodriguez <kungary04@gmail.com>
2023-06-14 03:45:57 -0500

    web-socket render

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:8080');
+const socket = io('https://chat-socket-io-nh4s.onrender.com');
 
 function App() {
 const [message, setMessage] = useState('');
